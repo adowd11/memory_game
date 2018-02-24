3dea9fba14c3135ade7ac698b2ed4c8fef7485e3
Addison Dowd <addisondowd@Addisons-MacBook-Pro.local>
2018-02-24 10:32:50 -0600

    finished modal, added js comments

--- js/app.js
+++ js/app.js
@@ -38,20 +38,25 @@ function shuffle(array) {
 
 newGame();
 
+//show and open classes toggled on when card is flipped
 function flipCard() {
   this.classList.toggle('show');
   this.classList.toggle('open');
 }
 
+//called from clicking on a card
 function showCard() {
+  //add the card to the end of the openCards array
   openCards.push(this);
+  //take away ability to click on an open card
   deactivateClick(openCards);
-  /*openCards[0].removeEventListener('click',flipCard);
-  openCards[0].removeEventListener('click',showCard);*/
   if (openCards.length === 2) {
+    //stop clicking ability on all cards when 2 are open
     deactivateClick(allCards);
     checkMatch();
+    //add a guess for each pair of cards guessed
     countGuess();
+    //start timer when the first 2 cards are open
     if (!timer){
       timer = setInterval(function(){ startTimer() }, 1000);
     }
@@ -59,27 +64,33 @@ function showCard() {
 }
 
 function checkMatch() {
+  //checking if data-ids match
   if (openCards[0].dataset.id === openCards[1].dataset.id) {
     matches();
   } else {
-    setTimeout(notMatches,500);
+    //cards remain open for 1 second
+    setTimeout(notMatches,1000);
   }
 }
 
 function matches() {
+  //add match class to matched cards and add them to matchedCards array
   for (var i=0; i<openCards.length; i++) {
     openCards[i].classList.toggle('match');
     matchedCards.push(openCards[i]);
   }
+  //once matchedCards has 16 the timer stops and the game ends
   if (matchedCards.length === 16) {
     clearInterval(timer);
     win();
   }
+  //reactivate the ability to click on cards
   activateClick(allCards);
   openCards = [];
 }
 
 function notMatches() {
+  //if cards don't match flip them over
   for (var i=0; i<openCards.length; i++) {
     openCards[i].classList.toggle('show');
     openCards[i].classList.toggle('open');
@@ -88,25 +99,29 @@ function notMatches() {
   openCards = []
 }
 
+//modal for when all cards are matched
 function win() {
-  var popup = document.getElementById('victoryWindow');
+  var victoryWindow = document.getElementById('victoryWindow');
   var yes = document.getElementById('yes');
   var no = document.getElementById('no');
-
-  popup.style.display = 'block';
-
+  var finalStars = document.getElementsByClassName('stars')[0];
+  var finalTimer = document.getElementsByClassName('timer')[0];
+  document.querySelector('.finalStars').innerHTML = finalStars.innerHTML;
+  document.querySelector('.finalTimer').innerHTML = finalTimer.innerHTML;
+  victoryWindow.style.display = 'block';
   yes.onclick = function() {
     newGame();
-    popup.style.display = 'none';
+    victoryWindow.style.display = 'none';
   }
   no.onclick = function() {
-    popup.style.display = 'none';
+    victoryWindow.style.display = 'none';
   }
 }
 
 function activateClick(array) {
   for (var i=0; i<array.length; i++) {
     var currentCard = array[i];
+    //only reactivate the ability to click on cards that aren't matched
     if(!currentCard.classList.contains('match')) {
       array[i].addEventListener('click',flipCard);
       array[i].addEventListener('click',showCard);
@@ -115,6 +130,7 @@ function activateClick(array) {
 }
 
 function deactivateClick(array) {
+  //take away ability to click on cards
   for (var i=0; i<array.length; i++) {
     array[i].removeEventListener('click',flipCard);
     array[i].removeEventListener('click',showCard);
@@ -124,25 +140,32 @@ function deactivateClick(array) {
 function countGuess() {
   guess++;
   document.querySelector('.moves').innerHTML = guess;
+  //3 stars for 12 or fewer guesses, 2 stars for 13-16 guesses, 1 star for 17+
   if (guess>12 && guess<17) {
     starList[0].classList.remove('fa-star');
     starList[0].classList.add('fa-star-o');
-  } else if (guess>16 && guess<21) {
+  } else if (guess>16 /*&& guess<21*/) {
     starList[1].classList.remove('fa-star');
     starList[1].classList.add('fa-star-o');
-  } else if (guess>20){
+  } /*else if (guess>20){
     starList[2].classList.remove('fa-star');
     starList[2].classList.add('fa-star-o');
-  }
+  }*/
 }
 
 function startTimer() {
   time++;
   min = Math.floor(time/60);
   sec = time%60;
+  //game ends if it takes longer than an hour
   if (min>59){
-    alert('You took over an hour on this..?\nGame. Over.')
-    newGame();
+    var timeoutWindow = document.getElementById('timeoutWindow');
+    var ok = document.getElementById('ok');
+    timeoutWindow.style.display = 'block';
+    ok.onclick = function () {
+      newGame();
+      timeoutWindow.style.display = 'none';
+    }
   } else {
     document.querySelector('.minutes').innerHTML = min;
     document.querySelector('.seconds').innerHTML = sec;
@@ -150,6 +173,7 @@ function startTimer() {
 }
 
 function newGame() {
+  //reset all classes on cards for a new game
   for (var i=0; i<card.length; i++) {
     allCards[i].classList.remove('show');
     allCards[i].classList.remove('open');
@@ -158,12 +182,14 @@ function newGame() {
   var shuffledCards = shuffle(allCards);
   var deckElement = document.getElementById('deck');
   deckElement.innerHTML = '';
+  //create array using the shuffled card results
   for (i=0; i<shuffledCards.length; i++) {
     deckElement.appendChild(shuffledCards[i]);
   }
   matchedCards = [];
   guess = 0;
   clearInterval(timer);
+  //set timer to undefined since timer start checks if !timer
   timer = undefined;
   time = 0;
   min = 0;
@@ -171,10 +197,12 @@ function newGame() {
   document.querySelector('.moves').innerHTML = guess;
   document.querySelector('.minutes').innerHTML = min;
   document.querySelector('.seconds').innerHTML = sec;
+  //reset stars to all full
   for (var i=0; i<starList.length; i++) {
     starList[i].classList.add('fa-star');
     starList[i].classList.remove('fa-star-o');
   }
+  //reset all cards to be able to be clicked on
   for (var i=0; i<allCards.length; i++) {
     allCards[i].addEventListener('click',flipCard);
     allCards[i].addEventListener('click',showCard);
